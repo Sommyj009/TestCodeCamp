9fd8efb91f63771af0fd9332078df765c867a18e
Sommyj009 <chisomjennifer@gmail.com>
2023-05-09 14:24:31 +0100

    Task 14 again

---
+++ firstJavaProject/src/Main.java
@@ -0,0 +1,12 @@
+// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press ⌥⏎ with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        System.out.println("I am excited to write my first Java code");
+        System.out.println("All the stressful days will count soon");
+        System.out.println("The self-development year");
+        }
+    }
